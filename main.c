aa55fa97ec4c28a44f12dc2e35737f84d528a514
Tchiarley-Alves <tchiarley@ucl.br>
2022-06-12 17:02:04 -0300

    Main_implementação

--- main.c
+++ main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "MinhaBiblioteca.h"
 
-int main(void) 
-{
+int main(void) {
   char entrada[30];
   char saida[30];
   char original[1];
   char novo[1];
   
+  printf("Digite: Uma palavra, um caracter desta palavra, um caracter substituto, repectivamente.\n");
+  scanf("%s", entrada);
+  scanf(" %c", original);
+  scanf(" %c", novo);
+
+  int quantidadeTrocas = codifica_mensagem(entrada, original[0], novo[0], saida);
+
+  printf("Sua palavra codificada: %s\n", saida);
+  printf("Sua palavra original: %s\n", entrada);
+  printf("Quantidade de trocas realizadas na palavra: %d\n", quantidadeTrocas);
+  
+  system("pause");
   return 0;
 }
